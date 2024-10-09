1ed2e4b59b408b2e9369a383b8422b82be7e7a98
Keith Capers <K.Capers94@gmail.com>
2024-10-08 22:27:11 -0400

    completed  lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word = "listen"):
+        self._match = word
+
+    def is_anagram(self,canidate):
+        return sorted(canidate) == sorted(self._match)
+    
+    def match(self, words):
+            return [word for word in words if self.is_anagram(word)]
+    
+    
